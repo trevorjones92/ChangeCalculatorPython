7f468ee460f85614aa1738a5960e06baa19ede4e
trevorjones92 <tkjones92@outlook.com>
2020-11-15 22:59:40 -0600

    Update pythonChangeCalculator.py

--- pythonChangeCalculator.py
+++ pythonChangeCalculator.py
@@ -1,3 +1,8 @@
+#The purpose of this code was to write a program with total change amount as an integer input, 
+#and output the change using the fewest coins, one coin type per line. 
+#The coin types are Dollars, Quarters, Dimes, Nickels, and Pennies. 
+#Use singular and plural coin names as appropriate, like 1 Penny vs. 2 Pennies.
+
 remainingChange = 0
 change = []
 moneyNames = ["Dollar","Dollars","Quarter","Quarters","Dime","Dimes","Nickel","Nickels","Penny","Pennies"]
@@ -22,7 +27,7 @@ for x in currency:
 print("Your change is ")
 counter = 0
 for i in change:
-    if i <= 1:
+    if i == 1:
         print(i, moneyNames[counter])
     else:
         print(i, moneyNames[counter + 1])
